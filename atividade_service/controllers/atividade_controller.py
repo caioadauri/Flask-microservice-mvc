d3e427d77db59b6ec395f3d1e2bba7fa793b2ea9
caioadauri <caioadaurigomes@gmail.com>
2024-11-21 22:22:27 -0300

    add route post put and delete

--- atividade_service/controllers/atividade_controller.py
+++ atividade_service/controllers/atividade_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from models import atividade_model
 from clients.pessoa_service_client import PessoaServiceClient
 
@@ -25,5 +25,33 @@ def obter_atividade_para_professor(id_atividade, id_professor):
             atividade = atividade.copy()
             atividade.pop('respostas', None)
         return jsonify(atividade)
+    except atividade_model.AtividadeNotFound:
+        return jsonify({'erro': 'Atividade não encontrada'}), 404
+
+@atividade_bp.route('/', methods=['POST'])
+def criar_atividade():
+    dados = request.get_json()
+    try:
+        nova_atividade = atividade_model.criar_atividade(dados)
+        return jsonify(nova_atividade), 201
+    except atividade_model.InvalidAtividadeData as e:
+        return jsonify({'erro': str(e)}), 400
+
+@atividade_bp.route('/<int:id_atividade>', methods=['PUT'])
+def atualizar_atividade(id_atividade):
+    dados = request.get_json()
+    try:
+        atividade_atualizada = atividade_model.atualizar_atividade(id_atividade, dados)
+        return jsonify(atividade_atualizada)
+    except atividade_model.AtividadeNotFound:
+        return jsonify({'erro': 'Atividade não encontrada'}), 404
+    except atividade_model.InvalidAtividadeData as e:
+        return jsonify({'erro': str(e)}), 400
+
+@atividade_bp.route('/<int:id_atividade>', methods=['DELETE'])
+def excluir_atividade(id_atividade):
+    try:
+        atividade_model.excluir_atividade(id_atividade)
+        return jsonify({'mensagem': 'Atividade excluída com sucesso'}), 200
     except atividade_model.AtividadeNotFound:
         return jsonify({'erro': 'Atividade não encontrada'}), 404
--- atividade_service/models/atividade_model.py
+++ atividade_service/models/atividade_model.py
@@ -22,6 +22,9 @@ atividades = [
 class AtividadeNotFound(Exception):
     pass
 
+class InvalidAtividadeData(Exception):
+    pass
+
 def listar_atividades():
     return atividades
 
@@ -30,3 +33,40 @@ def obter_atividade(id_atividade):
         if atividade['id_atividade'] == id_atividade:
             return atividade
     raise AtividadeNotFound
+
+def criar_atividade(dados):
+    if 'id_disciplina' not in dados or 'enunciado' not in dados or 'respostas' not in dados:
+        raise InvalidAtividadeData('Campos id_disciplina, enunciado e respostas são obrigatórios.')
+    nova_atividade = {
+        'id_atividade': gerar_novo_id(),
+        'id_disciplina': dados['id_disciplina'],
+        'enunciado': dados['enunciado'],
+        'respostas': dados.get('respostas', [])
+    }
+    atividades.append(nova_atividade)
+    return nova_atividade
+
+def atualizar_atividade(id_atividade, dados):
+    for i, atividade in enumerate(atividades):
+        if atividade['id_atividade'] == id_atividade:
+            if 'id_disciplina' in dados:
+                atividades[i]['id_disciplina'] = dados['id_disciplina']
+            if 'enunciado' in dados:
+                atividades[i]['enunciado'] = dados['enunciado']
+            if 'respostas' in dados:
+                atividades[i]['respostas'] = dados['respostas']
+            return atividades[i]
+    raise AtividadeNotFound
+
+def excluir_atividade(id_atividade):
+    for i, atividade in enumerate(atividades):
+        if atividade['id_atividade'] == id_atividade:
+            del atividades[i]
+            return
+    raise AtividadeNotFound
+
+def gerar_novo_id():
+    if atividades:
+        return max(atividade['id_atividade'] for atividade in atividades) + 1
+    else:
+        return 1
